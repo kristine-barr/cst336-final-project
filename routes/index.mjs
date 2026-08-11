ec65e65878f861d3728cbdbbe538a596d0bd136f
Steve Mendoza <smendoza88@gmail.com>
2026-08-10 19:21:14 -0700

    init wiring up openlib

--- routes/index.mjs
+++ routes/index.mjs
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import bookRouter from './book.mjs';
 import adminRouter from './admin.mjs';
+import openLibrary from './openLibrary.mjs'
 
 const router = Router();
 
 // Contains all the routes that will be used 
 router.use('/', bookRouter);
 router.use('/', adminRouter);
+router.use('/api', openLibrary)
 
 export default router;
